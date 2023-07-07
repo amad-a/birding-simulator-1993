e4b3ff1301cb49a494c11f88834f5334ea1fdd7c
Amad Ansari <amadansari@amad-m1.local>
2023-07-07 12:10:02 -0400

    let there be birds, zoom adjustment

--- birdingSim1993.js
+++ birdingSim1993.js
@@ -7,12 +7,12 @@ let simpleShader;
 let camera;
 let font;
 let shots = 24;
-let currentCoords = [0, 0];
+let currentCoords = [400, 400];
 let shotCoords,
   mirroredCoords = [0, 0];
 let screenWidth = 1280;
 let screenHeight = 960;
-let currentScreen = 2;
+let currentScreen = 8;
 let backgrounds;
 let cardinalDirections;
 let modelLoaded = false;
@@ -22,49 +22,6 @@ let savedTime;
 let totalTime = 1000;
 let cameraSound;
 
-// const screens = [
-//   {
-//     id: 0,
-//     url: '0.png',
-//     arrows: [
-//       { direction: 'w', x: 60, y: 450, screen: 1 },
-//       { direction: 'e', x: screenWidth - 185, y: 450, screen: 2 },
-//     ],
-//   },
-//   {
-//     id: 1,
-//     url: '1.png',
-//     arrows: [
-//       { direction: 'w', x: 60, y: 450, screen: 2 },
-//       { direction: 'e', x: screenWidth - 185, y: 450, screen: 0 },
-//     ],
-//   },
-//   {
-//     id: 2,
-//     url: '2.png',
-//     arrows: [
-//       { direction: 'w', x: 60, y: 450, screen: 0 },
-//       { direction: 'e', x: screenWidth - 185, y: 450, screen: 1 },
-//     ],
-//   },
-//   {
-//     id: 3,
-//     url: '3.png',
-//     arrows: [
-//       { direction: 'ne', x: 100, y: 100, screen: 2 },
-//       { direction: 'w', x: 100, y: 100, screen: 1 },
-//     ],
-//   },
-//   {
-//     id: 4,
-//     url: '4.png',
-//     arrows: [
-//       { direction: 'ne', x: 100, y: 100, screen: 1 },
-//       { direction: 'w', x: 100, y: 100, screen: 3 },
-//     ],
-//   },
-// ];
-
 const screens = [
   {
     id: 0,
@@ -72,16 +29,35 @@ const screens = [
       { direction: 'w', x: 60, y: 450, screen: 7 },
       { direction: 'e', x: screenWidth - 185, y: 450, screen: 11 },
     ],
+    birds: [
+      { bird: 'woodDuckS', x: 600, y: 500, chance: 1 },
+      { bird: 'osprey2S', x: 100, y: 100, chance: 1 },
+      { bird: 'crowFlyingS', x: 1000, y: 150, chance: 1 },
+    ],
   },
   {
     id: 1,
     arrows: [{ direction: 'w', x: 60, y: 450, screen: 2 }],
+    birds: [
+      { bird: 'herringGullXL', x: 300, y: 600, chance: 1 },
+      { bird: 'brantS', x: 850, y: 600, chance: 1 },
+      { bird: 'woodDuckS', x: 100, y: 600, chance: 1 },
+      { bird: 'hudsonianGodwitS', x: 300, y: 550, chance: 1 },
+      { bird: 'blueHeronFlyingS', x: 200, y: 100, chance: 1 },
+    ],
   },
   {
     id: 2,
     arrows: [
       { direction: 'w', x: 60, y: 450, screen: 3 },
       { direction: 'e', x: screenWidth - 185, y: 450, screen: 1 },
+    ],
+    birds: [
+      { bird: 'americanWhitePelicanS', x: 200, y: 100, chance: 1 },
+      { bird: 'blackSkimmerS', x: 600, y: 400, chance: 1 },
+      { bird: 'houseSparrowS', x: 800, y: 620, chance: 1 },
+      { bird: 'houseSparrowS', x: 900, y: 610, chance: 1 },
+      { bird: 'europeanStarlingS', x: 200, y: 660, chance: 1 },
     ],
   },
   {
@@ -90,6 +66,7 @@ const screens = [
       { direction: 'ne', x: 650, y: 500, screen: 10 },
       { direction: 'sw', x: 450, y: screenHeight - 150, screen: 2 },
     ],
+    birds: [{ bird: 'egretSittingL', x: 100, y: 700, chance: 1 }],
   },
   {
     id: 4,
@@ -98,26 +75,52 @@ const screens = [
       { direction: 'e', x: screenWidth - 185, y: 450, screen: 10 },
       { direction: 'n', x: 600, y: 40, screen: 5 },
     ],
+    birds: [
+      { bird: 'greatBlueHeronSittingL', x: 500, y: 300, chance: 1 },
+      { bird: 'beltedKingfisherS', x: 200, y: 600, chance: 1 },
+    ],
   },
   {
     id: 5,
     arrows: [
       { direction: 's', x: 600, y: screenHeight - 160, screen: 4 },
     ],
+    birds: [
+      // { bird: 'americanRobinS', x: 100, y: 400, chance: 1 },
+      { bird: 'redwingedBlackbirdS', x: 550, y: 100, chance: 1 },
+      { bird: 'northernCardinalS', x: 900, y: 300, chance: 1 },
+      { bird: 'europeanStarlingS', x: 850, y: 500, chance: 1 },
+      { bird: 'houseSparrowS', x: 550, y: 500, chance: 1 },
+    ],
   },
   {
     id: 6,
     arrows: [{ direction: 'w', x: 60, y: 450, screen: 10 }],
+    birds: [
+      { bird: 'mourningDoveXL', x: 100, y: 100, chance: 1 },
+      { bird: 'greatBlueHeron2M', x: 200, y: 200, chance: 1 },
+      { bird: 'americanWhitePelicanS', x: 300, y: 300, chance: 1 },
+    ],
   },
   {
     id: 7,
     arrows: [
       { direction: 'e', x: screenWidth - 185, y: 450, screen: 0 },
     ],
+    birds: [
+      { bird: 'blueHeronFlyingS', x: 120, y: 100, chance: 1 },
+      { bird: 'egretS', x: 850, y: 450, chance: 1 },
+    ],
   },
   {
     id: 8,
     arrows: [{ direction: 'sw', x: 60, y: 450, screen: 11 }],
+    birds: [
+      { bird: 'americanWhitePelicanM', x: 600, y: 100, chance: 1 },
+      { bird: 'monkParakeetS', x: 480, y: 400, chance: 1 },
+      { bird: 'northernCardinalS', x: 300, y: 650, chance: 1 },
+      { bird: 'americanCrow1S', x: 900, y: 360, chance: 1 },
+    ],
   },
   {
     id: 9,
@@ -130,6 +133,10 @@ const screens = [
         screen: 11,
       },
     ],
+    birds: [
+      { bird: 'americanCrow1M', x: 100, y: 520, chance: 1 },
+      { bird: 'snowEgretM', x: 650, y: 300, chance: 1 },
+    ],
   },
   {
     id: 10,
@@ -138,6 +145,11 @@ const screens = [
       { direction: 's', x: 600, y: screenHeight - 160, screen: 3 },
       { direction: 'e', x: screenWidth - 185, y: 450, screen: 6 },
     ],
+    birds: [
+      { bird: 'monkParakeetS', x: 100, y: 100, chance: 1 },
+      { bird: 'leastBitternM', x: 1000, y: 700, chance: 1 },
+      { bird: 'snowEgretFlyingS', x: 750, y: 700, chance: 1 },
+    ],
   },
   {
     id: 11,
@@ -145,6 +157,12 @@ const screens = [
       { direction: 's', x: 600, y: screenHeight - 160, screen: 9 },
       { direction: 'w', x: 60, y: 450, screen: 0 },
       { direction: 'ne', x: screenWidth - 185, y: 450, screen: 8 },
+    ],
+    birds: [
+      { bird: 'brantM', x: 850, y: 750, chance: 1 },
+      { bird: 'greatBlueHeronXL', x: 200, y: 600, chance: 1 },
+      { bird: 'egretXl', x: 100, y: 100, chance: 1 },
+      { bird: 'osprey2S', x: 100, y: 200, chance: 1 },
     ],
   },
 ];
@@ -164,6 +182,73 @@ function preload() {
     prev: loadImage('images/arrows/prev.png'),
   };
 
+  birdsList = {
+    woodDuckS: loadImage('images/birds/50/wood_duck.png'),
+    blueHeronFlyingS: loadImage(
+      'images/birds/50/great_blue_heron_3.png'
+    ),
+    hudsonianGodwitS: loadImage(
+      'images/birds/50/hudsonian_godwit.png'
+    ),
+    brantS: loadImage('images/birds/50/brant.png'),
+    crowFlyingS: loadImage('images/birds/50/crow_flying.png'),
+    osprey2S: loadImage('images/birds/50/osprey_2.png'),
+    ospreyS: loadImage('images/birds/50/osprey_1.png'),
+    americanWhitePelicanS: loadImage(
+      'images/birds/50/american_white_pelican.png'
+    ),
+    blackSkimmerS: loadImage('images/birds/50/black_skimmer.png'),
+    europeanStarlingS: loadImage(
+      'images/birds/50/european_starling.png'
+    ),
+    northernCardinalS: loadImage(
+      'images/birds/50/northern_cardinal.png'
+    ),
+    houseSparrowS: loadImage('images/birds/50/house_sparrow.png'),
+    beltedKingfisherS: loadImage(
+      'images/birds/50/belted_kingfisher.png'
+    ),
+    americanRobinS: loadImage('images/birds/50/american_robin.png'),
+    redwingedBlackbirdS: loadImage(
+      'images/birds/50/redwinged_blackbird.png'
+    ),
+    blackCappedChickadeeS: loadImage(
+      'images/birds/50/black_capped_chickadee.png'
+    ),
+    houseSparrowS: loadImage('images/birds/50/house_sparrow.png'),
+    egretS: loadImage('images/birds/50/egretSitting.png'),
+    monkParakeetS: loadImage('images/birds/100/monk_parakeet.png'),
+    americanCrow1S: loadImage('images/birds/50/american_crow_1.png'),
+    snowEgretFlyingS: loadImage('images/birds/50/snow_egret_2.png'),
+
+    woodDuckM: loadImage('images/birds/50/wood_duck.png'),
+    greatBlueHeron2M: loadImage(
+      'images/birds/100/great_blue_heron_2.png'
+    ),
+    americanWhitePelicanM: loadImage(
+      'images/birds/100/american_white_pelican.png'
+    ),
+    americanRobinM: loadImage('images/birds/100/american_robin.png'),
+    americanCrow1M: loadImage('images/birds/100/american_crow_1.png'),
+    leastBitternM: loadImage('images/birds/100/least_bittern.png'),
+    brantM: loadImage('images/birds/100/brant.png'),
+
+    egretSittingL: loadImage('images/birds/150/egret_sitting.png'),
+    greatBlueHeronSittingL: loadImage(
+      'images/birds/150/great_blue_heron.png'
+    ),
+    snowEgretM: loadImage('images/birds/100/snow_egret.png'),
+
+    herringGullXL: loadImage('images/birds/200/herring_gull.png'),
+    mourningDoveXL: loadImage(
+      'images/birds/200/mourningDoveFlying.png'
+    ),
+    greatBlueHeronXL: loadImage(
+      'images/birds/200/great_blue_heron.png'
+    ),
+    egretXl: loadImage('images/birds/200/egretSitting.png'),
+  };
+
   camera = loadImage('images/cameraTransparent2.png');
   roll = loadImage('images/35mmrollSharp.png');
   pointer = loadImage('images/pointer.png');
@@ -172,7 +257,6 @@ function preload() {
   blueheron = loadImage('images/birds/crowFlying.png');
   mallard = loadImage('images/birds/duck.png');
 
-  // delegate this to screens object?
   backgrounds = {
     0: loadImage('images/screens/0.png'),
     1: loadImage('images/screens/1.png'),
@@ -264,6 +348,20 @@ function renderArrows() {
   });
 }
 
+function renderBirds() {
+  let birds = screens[currentScreen].birds;
+  birds.map((bird) => {
+    birdGraphic = birdsList[bird.bird];
+    image(
+      birdGraphic,
+      bird.x,
+      bird.y,
+      birdGraphic.width * 2,
+      birdGraphic.height * 2
+    );
+  });
+}
+
 function overlapArrows() {
   let arrows = screens[currentScreen].arrows;
   let hoveredArrow;
@@ -283,6 +381,24 @@ function overlapArrows() {
   return hoveredArrow;
 }
 
+function birdDetected() {
+  let birds = screens[currentScreen].birds;
+  let hoveredArrow;
+  birds.map((bird) => {
+    birdGraphic = birdsList[bird.bird];
+    if (
+      calculateOverlap(
+        bird.x + 100,
+        bird.y,
+        birdGraphic.width * 2,
+        birdGraphic.height * 2
+      ) > 50
+    ) {
+      console.log('BIRD:', bird.bird);
+    }
+  });
+}
+
 function draw() {
   background(backgrounds[currentScreen]);
 
@@ -292,7 +408,6 @@ function draw() {
   textSize(130);
 
   if (modelLoaded && shots > 0) {
-    renderHud();
     let passedTime = millis() - savedTime;
 
     if (passedTime > totalTime && volume > 0) {
@@ -300,20 +415,9 @@ function draw() {
       savedTime = millis();
     }
 
-    // image(egret, 100, 300, egret.width * 1.5, egret.height * 1.5);
-
-    // image(
-    //   blueheron,
-    //   100,
-    //   100,
-    //   blueheron.width * 1.5,
-    //   blueheron.height * 1.5
-    // );
-
-    // image(mallard, 500, 450, mallard.width * 2, mallard.height * 2);
-
     renderArrows();
-    //renderBirds();
+    renderBirds();
+    renderHud();
 
     translate(width, 0);
     scale(-1, 1);
@@ -325,9 +429,6 @@ function draw() {
 }
 
 function drawCamera() {
-  if (!hands[0]) {
-  }
-
   if (modelLoaded) {
     for (let i = 0; i < hands.length; i += 1) {
       const prediction = hands[i];
@@ -362,6 +463,7 @@ function keyPressed() {
 
       grassSound.play();
     } else {
+      birdDetected();
       shots -= 1;
       shotCoords = currentCoords;
       volume = Math.min(12, volume + 6);
@@ -370,8 +472,6 @@ function keyPressed() {
     }
   }
 }
-
-function clickDetection() {}
 
 function calculateOverlap(x2, y2, w2, h2) {
   let x1 = currentCoords[0];
@@ -397,20 +497,3 @@ function calculateOverlap(x2, y2, w2, h2) {
 
   return percentageOverlap;
 }
-
-function successfulShot() {}
-function changeScreen() {}
-function arrowClick() {}
-// let screens = {};
-
-// function renderText(text, x, y, w, h) {
-//   text(shots, 135, 820, -100, 100);
-//   fill(255);
-//   text(shots, 145, 820, -100, 100);
-//   fill(0);
-
-//   text(volume, 135, 120, -100, 100);
-//   fill(255);
-//   text(volume, 145, 120, -100, 100);
-//   fill(0);
-// }
